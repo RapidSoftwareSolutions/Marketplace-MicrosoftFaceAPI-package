be5d126bfe23c01a6cc5c2b0a8b497fc7a91fb1b
Stefan Skliarov <stefan.skliarov@gmail.com>
2017-09-01 17:10:27 +0300

    api url changed on vendor

--- src/settings.php
+++ src/settings.php
@@ -16,6 +16,6 @@ return [
             'level' => \Monolog\Logger::DEBUG,
         ],
         
-        'api_url' => 'https://westus.api.cognitive.microsoft.com/face/v1.0/'
+        'api_url' => 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/'
     ],
 ];
